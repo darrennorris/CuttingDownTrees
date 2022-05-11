cd7f1b7108e69b45eb9517367e771c308fddc3ff
Darren Norris <doon75@hotmail.com>
2022-05-11 17:27:43 -0300

    check analysis update

--- R/check_analysis.R
+++ R/check_analysis.R
@@ -44,13 +44,72 @@ dfruns %>% select(run_id, muni_id) %>%
   crossing(year = 2006:2019) %>%
   left_join(dfgam_matched_model %>% 
               mutate(muni_id = as.character(muni_factor))) %>% 
-  data.frame() -> dftest # 1720278 rows 41*3 *999*14 = 1720278
+  arrange(run_id, cover_group) %>%
+  data.frame() -> dftest_random # 1720278 rows 41*3 *999*14 = 1720278
 
-#function holding model
-mygam_sal <- function(x) {
+#function holding models
+mygams <- function(x) {
   #run model for each randomized sample
   dftest <- x
 myctrl <- list(trace = TRUE)  
+
+#GVA
+bam_loss_gva <- bam(log_gva_percapita_reais ~ 
+                      cover_group +
+                      #Spatial smooth
+                      s(long, lat) + 
+                      #Spatial proximity
+                      s(dist_statecapital_km, state_namef, bs='fs', m=1) + 
+                      #Time
+                      s(year, state_namef, bs='fs', m=1) +
+                      s(yearf, bs = "re") +
+                      #Random 
+                      #s(cover_group, bs="re") +
+                      s(state_namef, bs="re") + 
+                      s(muni_factor, bs="re"), 
+                    #AR1 residual errors
+                    rho=0.874, AR.start = dftest$start_event, 
+                    family=Tweedie(1.99),
+                    method = "fREML",
+                    discrete = TRUE,
+                    data = dftest, 
+                    control = myctrl)   
+#get p values
+sumout_gva <- summary(bam_loss_gva)
+dft_gva <- as.data.frame(sumout_gva$p.pv)
+rm("bam_loss_gva")
+dfout_gva <- data.frame(response = "GVA", 
+                        var_name = row.names(dft_gva), pval = dft_gva[,1])
+
+#GDP
+bam_loss_01 <- bam(log_gdp_percapita_reais~ 
+                     cover_group +
+                     #Spatial smooth
+                     s(long, lat) + 
+                     #Spatial proximity
+                     s(dist_statecapital_km, state_namef, bs='fs', m=1) + 
+                     #Time
+                     s(year, state_namef, bs='fs', m=1) +
+                     #s(year, by = state_namef) +
+                     s(yearf, bs = "re") +
+                     #Random 
+                     s(state_namef, bs="re") + 
+                     #s(cover_group, bs="re") +
+                     s(muni_factor, bs="re"), 
+                   #AR1 residual errors
+                   rho=0.893, AR.start = dftest$start_event, 
+                   family=Tweedie(1.99),
+                   method = "fREML",
+                   discrete = TRUE,
+                   data = dftest, 
+                   control = myctrl)   
+#get p values
+sumout_gdp <- summary(bam_loss_01)
+dft_gdp <- as.data.frame(sumout_gdp$p.pv)
+rm("bam_loss_01")
+dfout_gdp <- data.frame(response = "GDP", 
+                        var_name = row.names(dft_gdp), pval = dft_gdp[,1])
+
 #Salary
 bam_loss_02 <- bam(log_min_salary_mean ~ 
                      cover_group +
@@ -73,20 +132,28 @@ bam_loss_02 <- bam(log_min_salary_mean ~
                    discrete = TRUE,
                    data = dftest, 
                    control = myctrl)   
-sumout <- summary(bam_loss_02)
-dft <- as.data.frame(sumout$p.pv)
+sumout_sal <- summary(bam_loss_02)
+dft_sal <- as.data.frame(sumout_sal$p.pv)
 rm("bam_loss_02")
-dfout <- data.frame(var_name = row.names(dft), pval = dft[,1])
+dfout_sal <- data.frame(response = "salary", 
+                        var_name = row.names(dft_sal), pval = dft_sal[,1])
+
+dfout <- rbind(dfout_gdp, dfout_gva, dfout_sal)
 dfout
 }
+#test with first 10 iterations
+rid_10 <- 1:((41*3*14)*10)
+dfcheck_10 <- plyr::ddply(dftest_random[rid_10, ], .(run_id) ,.fun = mygams)
 #test with first 100 iterations
-#rid_100 <- 1:((41*3*14)*100)
-#dfcheck_100 <- plyr::ddply(dftest[rid_100, ], .(run_id) ,.fun = mygam)
-dfcheck_sal <- plyr::ddply(dftest, .(run_id) ,.fun = mygam_sal)
-dfcheck_100 %>% 
+rid_100 <- 1:((41*3*14)*100)
+dfcheck_100 <- plyr::ddply(dftest[rid_100, ], .(run_id) ,.fun = mygams)
+#run model for each randomized sample
+dfcheck <- plyr::ddply(dftest, .(run_id) ,.fun = mygams)
+dfcheck_10 %>% 
   filter(!var_name== "(Intercept)") %>% 
   mutate(var_name = if_else(var_name=="cover_groupmore cover", 
                             "more cover", "more cover\nwith loss")) %>%
   ggplot(aes(x=pval)) + 
   geom_histogram() + 
-  facet_wrap(~var_name)
+  geom_vline(xintercept = 0.05) +
+  facet_grid(response~var_name)
